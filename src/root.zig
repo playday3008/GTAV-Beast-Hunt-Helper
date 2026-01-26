10f6a562abd8b950450d41584efb2197e271091b
PlayDay <18056374+playday3008@users.noreply.github.com>
2026-01-26 11:22:53 +0100

    fix: improve console allocation handling in DllMain

--- src/root.zig
+++ src/root.zig
@@ -6,6 +6,23 @@ const Hook = ScriptHookZig.Hook;
 
 const script = @import("script.zig");
 
+var console_thread: ?std.Thread = null;
+
+/// Tries to attach to an existing console, or allocates a new one.
+/// Runs in separate thread to avoid blocking the main thread.
+fn allocConsole() void {
+    // Try to attach to an existing console.
+    while (AttachConsole(windows.GetCurrentProcessId()) == 0) {
+        // If handle is invalid, there is no console to attach to.
+        if (windows.GetLastError() != windows.Win32Error.INVALID_HANDLE) {
+            break;
+        }
+
+        // Attach failed because there is no console, so allocate a new one.
+        _ = AllocConsole();
+    }
+}
+
 pub fn DllMain(
     hinstDLL: windows.HINSTANCE,
     fdwReason: windows.DWORD,
@@ -21,12 +38,15 @@ pub fn DllMain(
 
     switch (reason) {
         .PROCESS_ATTACH => {
-            _ = AllocConsole();
+            console_thread = std.Thread.spawn(.{}, allocConsole, .{}) catch null;
             Hook.scriptRegister(@ptrCast(hinstDLL), script.scriptMain);
         },
         .PROCESS_DETACH => {
             Hook.scriptUnregister(@ptrCast(hinstDLL));
-            _ = FreeConsole();
+            if (console_thread) |thread| {
+                _ = FreeConsole();
+                thread.join();
+            }
         },
         else => {},
     }
@@ -35,6 +55,8 @@ pub fn DllMain(
 }
 
 pub extern "kernel32" fn AllocConsole() callconv(.winapi) windows.BOOL;
+
+pub extern "kernel32" fn AttachConsole(dwProcessId: windows.DWORD) callconv(.winapi) windows.BOOL;
 
 pub extern "kernel32" fn FreeConsole() callconv(.winapi) windows.BOOL;
 
